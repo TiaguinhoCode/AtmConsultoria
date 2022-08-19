c7c21fe9c691d2610a34d6106bb8f4d19d503d85
TiaguinhoCode <tiago.rafael019@gmail.com>
2022-08-19 18:19:34 -0300

    Desativando o banner do Debug

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,6 @@ import 'package:atmconsultoria/TelaPrincipal.dart';
 void main(List<String> args) {
   runApp(MaterialApp(
     home: TelaPrincipal(),
+    debugShowCheckedModeBanner: false,
   ));
 }
