49fdc3bd262df0d4781f998dfb0fbaf364e7faa3
TiaguinhoCode <tiago.rafael019@gmail.com>
2022-08-19 12:28:12 -0300

    Desing finalizado da tela do cliente

--- lib/TelaCliente.dart
+++ lib/TelaCliente.dart
@@ -15,7 +15,38 @@ class _TelaClienteState extends State<TelaCliente> {
         title: Text("Cliente"),
         backgroundColor: Colors.green,
       ),
-      body: Container(),
+      body: Container(
+        margin: EdgeInsets.only(top: 20, left: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Image.asset(
+                  "images/detalhe_cliente.png",
+                  height: 100,
+                  fit: BoxFit.contain,
+                ),
+                Padding(padding: EdgeInsets.only(left: 10)),
+                Text(
+                  "Clientes",
+                  style: TextStyle(fontSize: 22),
+                )
+              ],
+            ),
+            Padding(padding: EdgeInsets.only(top: 25)),
+            Image.asset(
+              "images/cliente1.png",
+              fit: BoxFit.contain,
+              height: 55,
+            ),
+            Text("Empresa de Software"),
+            Padding(padding: EdgeInsets.only(top: 25)),
+            Image.asset("images/cliente2.png"),
+            Text("Empresa de auditoria")
+          ],
+        ),
+      ),
     );
   }
 }
